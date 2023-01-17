711e92561c7c9f3d5045362bcc16040b9eff574d
JNK <deal6789@naver.com>
2023-01-17 17:38:05 +0900

    day04 7.10

--- day04_mission.py
+++ day04_mission.py
@@ -1,23 +1,4 @@
-# 7.8
+# 7.10
 
-surprise = ["Groucho", "Chico", "Harpo"]
-print(surprise)
-
-# 7.9
-
-surprise[-1] = surprise[-1].lower()
-print(surprise)
-
-harpo_list = list(surprise[-1])
-print(harpo_list)
-
-harpo_list.reverse()
-print(harpo_list)
-
-harpo_list[0] = harpo_list[0].title()
-print(harpo_list)
-
-print(''.join(harpo_list))
-
-surprise[-1] = ''.join(harpo_list)
-print(surprise)
+even_number_list = [number for number in range(10) if number%2 == 0]
+print(even_number_list)
