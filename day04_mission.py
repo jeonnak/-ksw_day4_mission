1e8bc0e19b324c9d3b37f8427d838a2b721f1d80
JNK <deal6789@naver.com>
2023-01-17 18:44:56 +0900

    day04 8.1, 8.2, 8.3, 8.4, 8.5

--- day04_mission.py
+++ day04_mission.py
@@ -1,59 +1,22 @@
-# 7.11
+# 8.1
 
-start1 = ["fee", "fie", "foe"]
-rhymes = [
-    ("flop", "get a mop"),
-    ("fope", "turn the rope"),
-    ("fa", "get your ma"),
-    ("fudge", "call the judge"),
-    ("fat", "pet the cat"),
-    ("fog", "walk the dog"),
-    ("fun", "say we're done"),
-    ]
-start2 = "Someone better"
+e2f = {'dog': 'chien', 'cat': 'chat', 'walrus': 'morse'}
+print(e2f)
 
-# 첫 번째
+# 8.2
 
-# start1[0] = start1[0] + '!'
-# start1[1] = start1[1] + '!'
-# start1[2] = start1[2] + '!'
-# rhymes[0][0] = rhymes[0][0] + ' !'
-# rhymes[1][0] = rhymes[1][0] + ' !'
-# rhymes[2][0] = rhymes[2][0] + ' !'
-# rhymes[3][0] = rhymes[3][0] + ' !'
-# rhymes[4][0] = rhymes[4][0] + ' !'
-# rhymes[5][0] = rhymes[5][0] + ' !'
-# rhymes[6][0] = rhymes[6][0] + ' !'
+print({e2f['walrus']})
 
-start1[0] = start1[0].title()
-start1[1] = start1[1].title()
-start1[2] = start1[2].title()
+# 8.3
+f2e = {}
+for k, v in e2f.items():
+    f2e[v] = k
+print(f2e)
 
-print(rhymes[0]+rhymes[1]+rhymes[2]+rhymes[3]+rhymes[4]+rhymes[5]+rhymes[6])
+# 8.4
+print({f2e['chien']})
 
-# rhymes[0][0] = rhymes[0][0].title()
-# rhymes[1][0] = rhymes[1][0].title()
-# rhymes[2][0] = rhymes[2][0].title()
-# rhymes[3][0] = rhymes[3][0].title()
-# rhymes[4][0] = rhymes[4][0].title()
-# rhymes[5][0] = rhymes[5][0].title()
-# rhymes[6][0] = rhymes[6][0].title()
+#8.5
+for k in e2f.keys():
+    print(k)
 
-
-print(start1)
-print('! '.join(start1) + '! ' + rhymes[0][0] + '!')
-print()
-print('! '.join(start1) + '! ' + rhymes[1][0] + '!')
-
-print('! '.join(start1) + '! ' + rhymes[2][0] + '!')
-
-print('! '.join(start1) + '! ' + rhymes[3][0] + '!')
-
-print('! '.join(start1) + '! ' + rhymes[4][0] + '!')
-
-print('! '.join(start1) + '! ' + rhymes[5][0] + '!')
-
-print('! '.join(start1) + '! ' + rhymes[6][0] + '!')
-print(rhymes)
-
-print(rhymes[0][0])
